0c368c4b3aaf2860d19a46df0a56f18a5c2f4e8e
kimjae <o980204@naver.com>
2026-04-09 14:45:33 +0900

    commit

--- generated_report.py
+++ generated_report.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-with open("report.txt", "w" encoding="utf-8") as f:
+with open("report.txt", "w" ,encoding="utf-8") as f:
     f.write("github actions report\n")
     f.write(f"generated at: {datetime.now()}\n")
 
